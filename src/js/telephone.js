3770ae369c924b6bd5c37f4cc85b97f8c0d5e952
Igor Vazhenin <vidok0577@gmail.com>
2024-10-02 11:17:16 +0500

    change

--- src/js/telephone.js
+++ src/js/telephone.js
@@ -1,7 +1,5 @@
 export function normlizeTelephone(telephone) {
     const regExpPlus = /^8/;
     const regExpSplit = /[ |\-|(|)]/g;
-    // telephone = telephone.replace(regExpPlus, '+7');
-    // telephone = telephone.replace(regExpSplit, '');
     return telephone.replace(regExpPlus, '+7').replace(regExpSplit, '');
 }
